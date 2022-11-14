19dbbb3ca0812faacedf8b1d19bef193b1f26ed9
Florian Ludot <florian.ludot@gmail.com>
2022-11-14 20:29:13 +0200

    feat: demo

--- APNGKitPlayground/ViewController.swift
+++ APNGKitPlayground/ViewController.swift
@@ -5,15 +5,28 @@
 //  Created by Florian Ludot on 11/14/22.
 //
 
+import APNGKit
 import UIKit
 
 class ViewController: UIViewController {
+    private let image: APNGImageView = {
+        let img = try! APNGImage(named: "elephant")
+        let view = APNGImageView(image: img)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+        view.addSubview(image)
+
+        NSLayoutConstraint.activate([
+            image.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            image.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            image.widthAnchor.constraint(equalToConstant: 200),
+            image.heightAnchor.constraint(equalToConstant: 200)
+        ])
     }
-
-
 }
 
